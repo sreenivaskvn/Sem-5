dd999d0426165cc42c5733da2bb439ecf91cd254
sreenivaskvn <kvnsreenivas@gmail.com>
2016-08-14 23:30:07 +0530

    A simple signal-handler example

---
+++ oslab-cs333/lab3/signalhandler.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT)
+    printf("received SIGINT\n");
+}
+
+int main(void)
+{
+  if (signal(SIGINT, sig_handler) == SIG_ERR)
+  printf("\ncan't catch SIGINT\n");
+  // A long long wait so that we can easily issue a signal to this process
+  while(1) {
+  	printf("%s\n", "When you nod your head yes" );
+    sleep(1);
+  }
+  return 0;
+}
