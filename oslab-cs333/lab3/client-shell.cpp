0414b7d23cd9ad3d5edb10279b1c52273a040573
sreenivaskvn <kvnsreenivas@gmail.com>
2016-08-16 00:28:30 +0530

    Added 'getsq' and 'getpl'
    1) wait_thread function is a thread waiting in background to reap the get-one-sig-file process
    2) Added two lines to check if the user pressed enter without giving any command

--- oslab-cs333/lab3/client-shell.cpp
+++ oslab-cs333/lab3/client-shell.cpp
@@ -5,19 +5,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
 using namespace std;
 
+bool stop_cmd; // indicates whether to stop the 'getsq' and 'getpl' or not. We have to stop 
+			   // all the future downloads corresponding to that command.	      
 void sig_handler(int signo)
 {
   	if (signo == SIGINT){
-  		// Do nothing.
-  		// We want the parent i.e client-shell to keep on running even if a SIGINT is encountered. Hence we
-  		// need to overwrite.
-  		// But we need the child process to be killed.
+  		stop_cmd = true;
+  		// stop the current running command
 	}
+}
+
+void *wait_thread(void *i){
+	int pid = *((int*)i);
+	printf("%d\n", pid);
+    int status;
+    pid_t w = waitpid(pid, &status, 0);
 }
 
 class server_details{
@@ -78,12 +86,18 @@ int main()
     bool server_saved = false;
     if (signal(SIGINT, sig_handler) == SIG_ERR)
 	printf("\ncan't catch SIGINT\n");
+	pthread_t getpl_name[64];
+	int getpl_pid[64];
 
     while (1) {           
        
-    printf("\033[1;33mHello>\033[0m");     
+    printf("\033[1;33mHello>\033[0m");
+    	stop_cmd = false;    
         bzero(line, MAX_INPUT_SIZE);
         fgets(line, MAX_INPUT_SIZE, stdin);
+        if(strlen(line) == 1){
+        	continue;
+        }
         line[strlen(line)] = '\n'; //terminate with new line
         tokens = tokenize(line);
    		if(strcmp(tokens[0], "exit") == 0){
@@ -124,7 +138,7 @@ int main()
    				}
    				retfork = fork();
    				if(retfork == 0){
-   					execl("./get-one-file-sig","./get-one-file-sig",tokens[1],
+   					execl("./get-one-file-sig", "./get-one-file-sig", tokens[1],
    							 server.ip, server.port, "display", (char*)NULL);
    				}else{
    					int status;
@@ -147,12 +161,65 @@ int main()
         	continue;
         }
 
+        if(strcmp(tokens[0], "getsq") == 0){
+        	int num;
+        	for(i = 0; tokens[i] != NULL; i++);
+   			if(i < 2){
+   				printf("%s\n", "Usage : getsq <filename1> <filename2> ...");
+   			}else{
+   				if(!server_saved){
+   					printf("%s%s\n", "Server-ip and port have not been saved.\n",
+   						"To save : server <server-ip> <port>");
+   					continue;
+   				}
+   				for(num = 1; num < i && !stop_cmd; num++){
+	   				retfork = fork();
+	   				if(retfork == 0){
+	   					execl("./get-one-file-sig","./get-one-file-sig",tokens[num],
+	   							 server.ip, server.port, "nodisplay", (char*)NULL);
+	   				}else{
+	   					int status;
+	   					int x = waitpid(retfork, &status, 0);
+	   					printf("%d\n",x );
+	   				}
+	   			}
+   			}	
+   			continue;
+        }
+
+        if(strcmp(tokens[0], "getpl") == 0){
+        	int num;
+        	for(i = 0; tokens[i] != NULL; i++);
+   			if(i < 2){
+   				printf("%s\n", "Usage : getpl <filename1> <filename2> ...");
+   			}else{
+   				if(!server_saved){
+   					printf("%s%s\n", "Server-ip and port have not been saved.\n",
+   						"To save : server <server-ip> <port>");
+   					continue;
+   				}
+   				for(num = 1; num < i && !stop_cmd; num++){
+	   				getpl_pid[num-1] = fork();
+	   				if(getpl_pid[num-1] == 0){
+	   					execl("./get-one-file-sig","./get-one-file-sig",tokens[num],
+	   							 server.ip, server.port, "nodisplay", (char*)NULL);
+	   				}else{
+	   					pthread_create(&getpl_name[num-1], NULL, wait_thread, (void*)&getpl_pid[num-1]);
+	   				}
+	   			}
+	   			for(num = 1; num < i && !stop_cmd; num++){
+	   				pthread_join(getpl_name[num-1], NULL);
+	   			}
+   			}	
+   			continue;
+        }
+
         // Next if is for shell built-in's
         char *bin = "/bin/";
         char path_builtin[4+strlen(tokens[0])];
         strcpy(path_builtin, bin);
         strcat(path_builtin, tokens[0]);
-        printf("%s\n", path_builtin);
+        // printf("%s\n", path_builtin);
         if(exists(path_builtin)){
         	retfork = fork();
         	if(retfork == 0){
@@ -172,5 +239,7 @@ int main()
         }
        	free(tokens);
     } 
+     
+
 }
 
